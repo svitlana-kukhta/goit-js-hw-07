d91f7788c2fd182edfcd641e1e44ec752b5707d5
Svitlana <tyschenkosvetlana@gmail.com>
2024-09-13 15:55:11 +0300

    homework-7-js-2

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,8 @@
 'use strict'
 const nameInput = document.querySelector("#name-input");
 const nameOutput = document.querySelector("#name-output");
-nameInput.addEventListener("input", (event) => {nameOutput.textContent = event.currentTarget.value;})
+nameInput.addEventListener("input", (event) => {
+    const name = event.target.value.trim();
+    if (name !== "") {nameOutput.textContent = name;
+    } else { nameOutput.textContent = `Anonymous`; }}
+)
